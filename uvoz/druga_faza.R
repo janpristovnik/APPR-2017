44eb9270cb2249a63f2d3caa2c391623d846640e
Jan Pristovnik <jan.pristovnik@gmail.com>
2017-05-03 19:18:27 +0200

    Zbrisan View

--- uvoz/druga_faza.R
+++ uvoz/druga_faza.R
@@ -7,7 +7,7 @@ BDP["1"] <- NULL
 BDP["2"] <- NULL
 BDP["3"] <- NULL
 
-View(BDP)
+
 
 Tip_izobrazevanja <- read_csv("podatki/educ_uoe_enra01_1_Data.csv", col_names = c("Leto", "Drzava", "1", "2", "3", "4", "Stopnja_izobrazbe", "Stevilo", "5"),skip=1, na = ":",
                  locale = locale(encoding = "Windows-1250"))
@@ -17,12 +17,12 @@ Tip_izobrazevanja["2"] <- NULL
 Tip_izobrazevanja["3"] <- NULL
 Tip_izobrazevanja["4"] <- NULL
 Tip_izobrazevanja["5"] <- NULL
-View(Tip_izobrazevanja)
+
 
 stevilo_prebivalcev <- read_csv("podatki/demo_gind_1_Data.csv", col_names = c("Leto", "Drzava", "1", "Stevilo", "2"), skip = 1)
 stevilo_prebivalcev["1"] <- NULL
 stevilo_prebivalcev["2"] <- NULL
-View(stevilo_prebivalcev)
+
 
 library(rvest)
 library(gsubfn)
@@ -38,7 +38,7 @@ delez.za.izobrazevanje.tidy <- melt(delez.za.izobrazevanje, id.vars="Drzava", me
 
 colnames(delez.za.izobrazevanje.tidy)<- c("Drzava", "Leto", "delez_BDP_za_izobrazbo")
 
-View(delez.za.izobrazevanje.tidy)
+
 
 
  
