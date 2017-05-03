861a9f104082ed8e7875cb979c02967fa1b03edb
Jan Pristovnik <jan.pristovnik@gmail.com>
2017-05-03 12:30:29 +0200

    .

--- uvoz/druga_faza.R
+++ uvoz/druga_faza.R
@@ -1,6 +1,6 @@
 
 library(readr)
-BDP <- read_csv("nama_10_gdp_1_Data.csv", col_names = c("Leto", "Drzava", "1", "2", "Milijoni evrov", "3"), na = ":",
+BDP <- read_csv("podatki/nama_10_gdp_1_Data.csv", col_names = c("Leto", "Drzava", "1", "2", "Milijoni evrov", "3"), na = ":",
                 locale = locale(encoding = "Windows-1250"))
 
 BDP["1"] <- NULL
@@ -9,7 +9,7 @@ BDP["3"] <- NULL
 
 View(BDP)
 
-data <- read_csv("educ_uoe_enra01_1_Data.csv", col_names = c("Leto", "Drzava", "1", "2", "3", "4", "Stopnja izobrazbe", "Stevilo", "5"), na = ":",
+data <- read_csv("podatki/educ_uoe_enra01_1_Data.csv", col_names = c("Leto", "Drzava", "1", "2", "3", "4", "Stopnja izobrazbe", "Stevilo", "5"), na = ":",
                  locale = locale(encoding = "Windows-1250"))
 
 data["1"] <- NULL
