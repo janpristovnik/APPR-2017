089aeff4269474afc6c14030a1ac78c75949540e
Jan Pristovnik <jan.pristovnik@gmail.com>
2017-05-03 16:56:40 +0200

    Brisanje napacnih podatkov in sprememba imena stolpca

--- uvoz/druga_faza.R
+++ uvoz/druga_faza.R
@@ -36,7 +36,7 @@ colnames(delez.za.izobrazevanje) <- c("Drzava", 2013:2015)
 
 delez.za.izobrazevanje.tidy <- melt(delez.za.izobrazevanje, id.vars="Drzava", measure.vars = names(delez.za.izobrazevanje)[-1], variable.name = "leto", value.name = "delez BDPja namenjen za izobrazevanje",na.rm = TRUE)
 
-colnames(delez.za.izobrazevanje.tidy)<- c("Drzava", "leto", "% BDP za izobrazbo")
+colnames(delez.za.izobrazevanje.tidy)<- c("Drzava", "Leto", "% BDP za izobrazbo")
 View(delez.za.izobrazevanje.tidy)
 
 
