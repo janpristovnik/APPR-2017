f74b59b9e7688dc5ad71c99083ac15e2dddf22b7
Jan Pristovnik <jan.pristovnik@gmail.com>
2017-05-22 16:43:24 +0200

    drugi graf

--- uvoz/druga_faza.R
+++ uvoz/druga_faza.R
@@ -42,19 +42,21 @@ colnames(delez.za.izobrazevanje) <- c("Drzava", 2013:2015)
 delez.za.izobrazevanje.tidy <- melt(delez.za.izobrazevanje, id.vars="Drzava", measure.vars = names(delez.za.izobrazevanje)[-1], variable.name = "leto", value.name = "delez BDPja namenjen za izobrazevanje",na.rm = TRUE)
 
 colnames(delez.za.izobrazevanje.tidy)<- c("Drzava", "Leto", "delez_BDP_za_izobrazbo")
+delez.za.izobrazevanje.tidy$Leto <- parse_number(delez.za.izobrazevanje.tidy$Leto)
+delez.za.izobrazevanje.tidy$delez_BDP_za_izobrazbo <- parse_number(delez.za.izobrazevanje.tidy$delez_BDP_za_izobrazbo)
 
 #tretja_faza_vizualizacija
 
 izbrane_drzave_izo <- Tip_izobrazevanja %>% filter(Drzava=="Italy" | Drzava =="Spain" | Drzava == "Germany (until 1990 former territory of the FRG)" | Drzava == "Malta" | Drzava =="Slovenia" | Drzava == "Croatia"| Drzava == "Sweden" )
 izbrane_drzave_stevilo <- stevilo_prebivalcev %>% filter(Drzava=="Italy" | Drzava =="Spain" | Drzava == "Germany (until 1990 former territory of the FRG)" | Drzava == "Malta" | Drzava =="Slovenia" | Drzava == "Croatia"| Drzava == "Sweden" )
-
+izbrane_drzave_delez_BDP <- delez.za.izobrazevanje.tidy %>% filter(Drzava=="Italy" | Drzava =="Spain" | Drzava == "Germany (until 1990 former territory of the FRG)" | Drzava == "Malta" | Drzava =="Slovenia" | Drzava == "Croatia"| Drzava == "Sweden" )
 izbrane_drzave_izo_vsota <- izbrane_drzave_izo %>% group_by(Leto, Drzava) %>% summarise(Stevilo_koncanih = sum(Stevilo_koncanih))
 zdruzeno <- left_join(izbrane_drzave_izo_vsota, izbrane_drzave_stevilo)
 
 library(ggplot2)
 library(dplyr)
 prvi_graf <- ggplot(zdruzeno) + aes(x = Drzava, y = Stevilo_koncanih/Stevilo_preb) + geom_point() + xlab("Drzave") + ylab("Stevilo koncanih izobrazb na prebivalca")
-
+drugi_graf <- ggplot(izbrane_drzave_delez_BDP) + aes(x = Drzava, y = delez_BDP_za_izobrazbo) + geom_point()
 
 
 
