19a46f153b0c9ca179607c6fc33513ab0a75439a
Jan <jan.pristovnik@gmail.com>
2017-08-25 07:17:33 +0200

    Shiny popravek

--- shiny/server.R
+++ shiny/server.R
@@ -2,7 +2,7 @@ library(shiny)
 shinyServer(function(input,output) {
   output$lin <- renderPlot({
     data <- filter(zdruzeno_tretji_graf,
-                   Drzava == input$Drzava,
+                   Drzava %in% input$Drzava,
                    Leto == input$Leto
                    
                    
@@ -10,7 +10,7 @@ shinyServer(function(input,output) {
                    
     )
     
-    g3 <- ggplot(data, aes(x = Drzava, y = Stevilo_koncanih/Stevilo_preb, fill = Stopnja_izobrazbe))+ +
+   ggplot(data, aes(x = Drzava, y = Stevilo_koncanih/Stevilo_preb, fill = Stopnja_izobrazbe))+ 
       geom_col(position = "dodge")
   })
 }
